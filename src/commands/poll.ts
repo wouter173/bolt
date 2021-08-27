d861b8a0ce592ffa502c948a33dff01b9c53f2b8
wouter <31807225+wouter173@users.noreply.github.com>
2021-08-27 19:59:50 +0200

    made it so you can only use a poll once

--- src/commands/poll.ts
+++ src/commands/poll.ts
@@ -53,6 +53,23 @@ export default async (cmd: Command): Promise<Response> => {
 export const vouch = async (comp: Component, min: boolean): Promise<Response> => {
     var val = (parseInt(comp.message.embeds[0].fields![min? 1: 0].value)) + 1
     var total = parseInt(comp.message.embeds[0].fields![2].value)
+
+	let v = await SCORE.get(comp.message.id)
+	let real_v = (v == null? '': v)
+	let users = real_v.split(',')
+
+	if (users.includes(comp.member.user.id)) {
+		return new Response(JSON.stringify({
+			type: 7,
+			data: {
+				embeds: [comp.message.embeds[0]],
+			}
+		}), {headers:{'content-type': 'application/json'}})
+	}
+
+	users = [...users, comp.member.user.id]
+	await SCORE.put(comp.message.id, users.join(','))
+
     comp.message.embeds[0].fields![min ? 1 : 0].value = val + '';
     comp.message.embeds[0].fields![2].value = (total + (min? -1: 1))+'';
 
