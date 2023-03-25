78bfa9160f7df3018ba96a4041de45d1459d7b26
wouter <wouter@debruijn.dev>
2023-03-25 02:14:27 +0100

    Add logging of generated image.

--- src/commands/dayssince.tsx
+++ src/commands/dayssince.tsx
@@ -43,6 +43,7 @@ export default async (cmd: Command, url: URL): Promise<Response> => {
 	);
 
 	const img = await generateImage(template, { width: 612, height: 477 });
+	console.log(img);
 	const body = JSON.stringify({
 		type: 4,
 		data: {
